5092c22eabe07c154e6be48ebab90bf614c24457
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-18 18:10:23 -0300

    add anotacao column e min

--- src/main/java/br/com/xyinc/persistence/entity/Estabelecimento.java
+++ src/main/java/br/com/xyinc/persistence/entity/Estabelecimento.java
@@ -5,7 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
 
 /**
  * Criado por @autor wcardoso2  em 3/18/2019
@@ -14,12 +17,18 @@ import javax.persistence.Entity;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
+@Table(name = "estabelecimento")
 public class Estabelecimento extends AbstractEntity {
 
+    @Column(name = "nome", nullable = false)
     private String nome;
 
+    @Min(0)
+    @Column(name = "posicao_x", nullable = false)
     private Long posicaoX;
 
+    @Min(0)
+    @Column(name = "posicao_x", nullable = false)
     private Long posicaoY;
 
     @Builder
