67e936cac549fc7866612d204972e3941716e8a2
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-18 19:23:29 -0300

    criando classe service

---
+++ src/main/java/br/com/xyinc/service/IEstabelecimentoService.java
@@ -0,0 +1,7 @@
+package br.com.xyinc.service;
+
+/**
+ * Criado por @autor wcardoso2  em 3/18/2019
+ **/
+public interface IEstabelecimentoService {
+}
---
+++ src/main/java/br/com/xyinc/service/impl/EstabelecimentoService.java
@@ -0,0 +1,11 @@
+package br.com.xyinc.service.impl;
+
+import br.com.xyinc.service.IEstabelecimentoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Criado por @autor wcardoso2  em 3/18/2019
+ **/
+@Service
+public class EstabelecimentoService implements IEstabelecimentoService {
+}
