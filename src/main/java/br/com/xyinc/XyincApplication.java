41c26a7311dbb7fd588bc9e4dfc950101fc92c02
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-18 17:23:12 -0300

    Criação do projeto Inicial

---
+++ src/main/java/br/com/xyinc/XyincApplication.java
@@ -0,0 +1,13 @@
+package br.com.xyinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyincApplication.class, args);
+    }
+
+}
