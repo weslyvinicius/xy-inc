0dc2d022cf5a7a1c348716a06256a872188693c6
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-18 19:23:12 -0300

    renomeando arquivo

--- src/main/java/br/com/xyinc/persistence/repository/IEstabelecimentoRepository.java
+++ src/main/java/br/com/xyinc/persistence/repository/IEstabelecimentoRepository.java
@@ -2,9 +2,11 @@ package br.com.xyinc.persistence.repository;
 
 import br.com.xyinc.persistence.entity.Estabelecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Criado por @autor wcardoso2  em 3/18/2019
  **/
-public interface estabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
+@Repository
+public interface IEstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
 }
