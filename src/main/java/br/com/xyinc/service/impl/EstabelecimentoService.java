a7ecff4dc879e0df0588e155206195ca0ef7d1fb
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-19 15:34:35 -0300

    criando classe de serviço da busca de estabelecimentos por raio

--- src/main/java/br/com/xyinc/service/impl/EstabelecimentoService.java
+++ src/main/java/br/com/xyinc/service/impl/EstabelecimentoService.java
@@ -2,6 +2,7 @@ package br.com.xyinc.service.impl;
 
 import br.com.xyinc.persistence.entity.Estabelecimento;
 import br.com.xyinc.persistence.repository.IEstabelecimentoRepository;
+import br.com.xyinc.recurso.v1.dto.EstabelecimentoPorDistanciaDTO;
 import br.com.xyinc.service.IEstabelecimentoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +22,32 @@ public class EstabelecimentoService implements IEstabelecimentoService {
 
     @Override
     public List<Estabelecimento> listAll() {
-        log.info("Inicio do metodo service de listar todos os estabelecimentos");
+        log.info("Início do metodo service de listar todos os estabelecimentos");
         return estabelecimentoRepository.findAll();
     }
 
     @Override
     public Estabelecimento salvar(final Estabelecimento estabelecimento) {
-        log.info("Inicio do metodo service salvar um estabelecimento");
-        Estabelecimento estabelecimentoSalvo = estabelecimentoRepository.save(estabelecimento);
+        log.info("Início do metodo service salvar um estabelecimento");
+
+        Estabelecimento estabelecimentoSalvo;
+
+        log.info("Verifica se o estabelecimento já existe na mesma posicao X e Y");
+        Estabelecimento estabelecimentoJaExitente =  estabelecimentoRepository.findByPosicaoXAndPosicaoY(estabelecimento.getPosicaoX(), estabelecimento.getPosicaoY() );
+        if (estabelecimentoJaExitente != null){
+            log.info("Caso já exista o cadastro na mesma posicao atualizado o estabelecimento.");
+            estabelecimentoJaExitente.setNome(estabelecimento.getNome());
+            estabelecimentoSalvo = estabelecimentoRepository.save(estabelecimentoJaExitente);
+        }else{
+            estabelecimentoSalvo = estabelecimentoRepository.save(estabelecimento);
+        }
+
         return estabelecimentoSalvo;
+    }
+
+    @Override
+    public List<Estabelecimento> buscaEstabelecimentosPorRaio(final EstabelecimentoPorDistanciaDTO estabelecimentoPorDistanciaDTO) {
+        log.info("Início do metodo service de busca estabelecimento por raio");
+        return estabelecimentoRepository.buscaEstabelecimentoPorRaio(estabelecimentoPorDistanciaDTO.getPosicaoX(), estabelecimentoPorDistanciaDTO.getPosicaoY(), estabelecimentoPorDistanciaDTO.getDistanciaMaxima());
     }
 }
