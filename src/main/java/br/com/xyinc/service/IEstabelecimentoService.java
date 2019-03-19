b685fe0ec413e0d1fbfd7a61c8c09b0553427888
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-18 23:13:09 -0300

    criando o serviço de buscar todos os estabelecimentos

--- src/main/java/br/com/xyinc/service/IEstabelecimentoService.java
+++ src/main/java/br/com/xyinc/service/IEstabelecimentoService.java
@@ -1,7 +1,17 @@
 package br.com.xyinc.service;
 
+import br.com.xyinc.persistence.entity.Estabelecimento;
+
+import java.util.List;
+
 /**
  * Criado por @autor wcardoso2  em 3/18/2019
  **/
 public interface IEstabelecimentoService {
+
+    /**
+     * Serviço de busca todos os estabelecimentos cadastrados.
+     * @return Lista de Estabelecimentos
+     */
+    List<Estabelecimento> listAll();
 }
--- src/main/java/br/com/xyinc/service/impl/EstabelecimentoService.java
+++ src/main/java/br/com/xyinc/service/impl/EstabelecimentoService.java
@@ -1,11 +1,27 @@
 package br.com.xyinc.service.impl;
 
+import br.com.xyinc.persistence.entity.Estabelecimento;
+import br.com.xyinc.persistence.repository.IEstabelecimentoRepository;
 import br.com.xyinc.service.IEstabelecimentoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Criado por @autor wcardoso2  em 3/18/2019
  **/
 @Service
+@Slf4j
 public class EstabelecimentoService implements IEstabelecimentoService {
+
+    @Autowired
+    IEstabelecimentoRepository estabelecimentoRepository;
+
+    @Override
+    public List<Estabelecimento> listAll() {
+        log.info("Inicio do metodo service de listar todos os estabelecimentos");
+        return estabelecimentoRepository.findAll();
+    }
 }
