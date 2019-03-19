ad75ceb98cb6ba75ce879cedf5fd8bb36f34b41f
weslyvinicius <weslyvinicius@hotmail.com>
2019-03-19 00:10:46 -0300

    criando arquivo de detales de resposta

---
+++ src/main/java/br/com/xyinc/exception/handeler/ResponseHandlerDetail.java
@@ -0,0 +1,31 @@
+package br.com.xyinc.exception.handeler;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Getter
+@Builder
+public class ResponseHandlerDetail {
+
+    private Integer status;
+    private LocalDateTime datetime = LocalDateTime.now();
+    private String classeDeError;
+    private List<String> errosMensagem;
+    private Map<String, String> listaCamposObrigatorios;
+
+    public ResponseHandlerDetail addMenssagemErro(final String messagem) {
+        this.errosMensagem = Optional.ofNullable(this.errosMensagem).orElse(new ArrayList<>());
+        this.errosMensagem.add(messagem);
+        return this;
+    }
+
+    public ResponseHandlerDetail addCamposObrigarios(Map<String, String> listCamposObrigarios) {
+        this.listaCamposObrigatorios = Optional.ofNullable(this.listaCamposObrigatorios).orElse(new HashMap<>());
+        this.listaCamposObrigatorios.putAll(listCamposObrigarios);
+        return this;
+    }
+
+}
